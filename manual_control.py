5c107e33b4fa805c5ffdf2fdff908464e6447344
kxlv2000 <49295281+kxlv2000@users.noreply.github.com>
2023-06-14 16:33:36 +0800

    Update manual_control.py

--- manual_control.py
+++ manual_control.py
@@ -1363,7 +1363,7 @@ def game_loop(args):
         client.set_timeout(20.0)
 
         # sim_world = client.get_world()
-        sim_world=client.load_world('Town10_Opt')
+        sim_world=client.load_world('Town01_Opt')
         sim_world.unload_map_layer(carla.MapLayer.Foliage)
 
         if args.sync:
